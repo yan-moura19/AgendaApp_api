96bf542b245fcee718df7ce0db24bf97b1507e54
Yan Gabriel <43892743+yan-moura19@users.noreply.github.com>
2024-07-04 09:42:05 -0300

    cors

--- index.js
+++ index.js
@@ -8,8 +8,15 @@ const userRoutes = require('./routes/userRoutes');
 const app = express();
 const port = 3000;
 
+const corsOptions = {
+  origin: '*', 
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], 
+  allowedHeaders: ['Content-Type', 'Authorization'], 
+};
+
+
 app.use(bodyParser.json());
-app.use(cors())
+app.use(cors(corsOptions))
 
 mongoose.connect('mongodb+srv://yangabriel3301:7HOQBaxv9ymi6uvT@cluster0.wg6bcpg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
   useNewUrlParser: true,
